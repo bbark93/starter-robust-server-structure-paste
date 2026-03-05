48ce72ae25088b4411b8b86f84859f9b74b4e26b
Brandon Barksdale <bbark93@gmail.com>
2026-03-05 15:14:21 -0500

    methodNotAllowed.js

--- src/users/users.router.js
+++ src/users/users.router.js
@@ -1,9 +1,8 @@
 const router = require("express").Router();
-
 const controller = require("./users.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/:userId").get(controller.read);
-
-router.route("/").get(controller.list);
+router.route("/:userId").get(controller.read).all(methodNotAllowed);
+router.route("/").get(controller.list).all(methodNotAllowed);
 
 module.exports = router;
