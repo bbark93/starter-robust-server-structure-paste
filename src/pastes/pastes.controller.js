e2c4c6e94fa480a9cfc2eb5c9361ea21dee60c7a
Brandon Barksdale <bbark93@gmail.com>
2026-03-05 13:56:41 -0500

    res.local.paste and users-data.js

--- src/pastes/pastes.controller.js
+++ src/pastes/pastes.controller.js
@@ -20,6 +20,7 @@ function pasteExists(req, res, next) {
   const { pasteId } = req.params;
   const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
   if (foundPaste) {
+    res.locals.paste = foundPaste;
     return next();
   }
   next({
@@ -29,9 +30,7 @@ function pasteExists(req, res, next) {
 }
 
 function read(req, res) {
-  const { pasteId } = req.params;
-  const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
-  res.json({ data: foundPaste });
+  res.json({ data: res.locals.paste });
 }
 
 function create(req, res) {
@@ -94,18 +93,17 @@ function expirationIsValidNumber(req, res, next) {
 }
 
 function update(req, res) {
-  const { pasteId } = req.params;
-  const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
+  const paste = res.locals.paste
   const { data: { name, syntax, expiration, exposure, text } = {} } = req.body;
 
   // Update the paste
-  foundPaste.name = name;
-  foundPaste.syntax = syntax;
-  foundPaste.expiration = expiration;
-  foundPaste.exposure = exposure;
-  foundPaste.text = text;
+  paste.name = name;
+  paste.syntax = syntax;
+  paste.expiration = expiration;
+  paste.exposure = exposure;
+  paste.text = text;
 
-  res.json({ data: foundPaste });
+  res.json({ data: paste });
 }
 
 function destroy(req, res) {
